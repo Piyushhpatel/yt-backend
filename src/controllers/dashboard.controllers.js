6720b3fd5e4e1baa2b82d6aa0f393a87eced4f76
Piyush Patel <piyushpatel1746@gmail.com>
2024-09-19 22:56:54 +0530

    Added dashboard and like controllers

--- src/controllers/dashboard.controllers.js
+++ src/controllers/dashboard.controllers.js
@@ -1,17 +1,77 @@
 import mongoose from "mongoose"
 import {Video} from "../models/video.model.js"
 import {Subscription} from "../models/subscription.model.js"
-import {Like} from "../models/like.model.js"
-import {ApiError} from "../utils/ApiError.js"
-import {ApiResponse} from "../utils/ApiResponse.js"
+import {User} from "../models/user.model.js"
+import {Like} from "../models/like.models.js"
+import {ApiError} from "../utils/AppError.js"
+import {ApiResponse} from "../utils/AppResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
 
 const getChannelStats = asyncHandler(async (req, res) => {
     // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
+    const stats = await User.aggregate([
+        {
+            $match: {
+                _id: new mongoose.Types.ObjectId(req.user?._id),
+            }
+        },
+        {
+            $lookup: {
+                from: "subscriptions",
+                localField: "_id",
+                foreignField: "channel",
+                as: "subscribers",
+            }
+        },
+        {
+            $lookup: {
+                from: "videos",
+                localField: "_id",
+                foreignField: "owner",
+                as: "videos"
+            }
+        },
+        {
+            $addFields: {
+                subscriberCount: {
+                    $size: "$subscribers",
+                },
+                videoCount: {
+                    $size: "$videos",
+                },
+            }
+        },
+        {
+            $project: {
+                fullname: 1,
+                username: 1,
+                avatar: 1,
+                coverImage: 1,
+                subscriberCount: 1,
+                videoCount: 1
+            }
+        }
+    ]);
+
+    if(!stats){
+        throw new ApiError(404, "Channel Not Found");
+    }
+
+    return res.status(200).json(
+        new ApiResponse(200, stats[0], "Channel Fetched Succesfully")
+    )
 })
 
 const getChannelVideos = asyncHandler(async (req, res) => {
-    // TODO: Get all the videos uploaded by the channel
+    const videos = await Video.find({owner: req.user?._id});
+
+    if(!videos) {
+        throw new ApiError(404, "Videos created by user not found");
+    }
+
+    return res.status(200).json(
+        new ApiResponse(200, videos, "Video Fethched Successfully")
+    )
 })
 
 export { getChannelStats, getChannelVideos }
--- src/controllers/like.controllers.js
+++ src/controllers/like.controllers.js
@@ -1,33 +1,124 @@
-import mongoose, {isValidObjectId} from "mongoose"
-import {Like} from "../models/like.model.js"
-import {ApiError} from "../utils/ApiError.js"
-import {ApiResponse} from "../utils/ApiResponse.js"
-import {asyncHandler} from "../utils/asyncHandler.js"
+import mongoose, { isValidObjectId } from "mongoose";
+import { Like } from "../models/like.models.js";
+import { ApiError } from "../utils/AppError.js";
+import { ApiResponse } from "../utils/AppResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { Video } from "../models/video.model.js";
+import { Comment } from "../models/comments.model.js";
+import { Tweet } from "../models/tweet.model.js";
 
 const toggleVideoLike = asyncHandler(async (req, res) => {
-    const {videoId} = req.params
-    //TODO: toggle like on video
-})
+  const { videoId } = req.params;
+
+  const video = await Video.findById(videoId);
+
+  if (!video) {
+    throw new ApiError(404, "Video not found");
+  }
+
+  const existingLike = await Like.findOne({
+    video: videoId,
+    likedBy: req.user?._id,
+  });
+
+  if(existingLike) {
+    await Like.findOneAndDelete({video: videoId});
+    return res.status(200).json(new ApiResponse(200, "Video Unliked"));
+  }
+  else{
+    const like = await Like.create({
+        video: videoId,
+        likedBy: req.user?._id
+    });
+    return res.status(200).json(new ApiResponse(200, like, "Video Liked"));
+  }
+});
 
 const toggleCommentLike = asyncHandler(async (req, res) => {
-    const {commentId} = req.params
-    //TODO: toggle like on comment
+  const { commentId } = req.params;
 
-})
+  const comment = await Comment.findById(commentId);
+
+  if (!comment) {
+    throw new ApiError(404, "Comment not found");
+  }
+
+  const existingLike = await Like.findOne({
+    comment: commentId,
+    likedBy: req.user?._id,
+  });
+
+  if(existingLike) {
+    await Like.findOneAndDelete({comment: commentId});
+    return res.status(200).json(new ApiResponse(200, "Comment Unliked"));
+  }
+  else{
+    const like = await Like.create({
+        comment: commentId,
+        likedBy: req.user?._id
+    });
+    return res.status(200).json(new ApiResponse(200, like, "Comment Liked"));
+  }
+});
 
 const toggleTweetLike = asyncHandler(async (req, res) => {
-    const {tweetId} = req.params
-    //TODO: toggle like on tweet
-}
-)
+  const { tweetId } = req.params;
+
+  const tweet = await Tweet.findById(tweetId);
+
+  if (!tweet) {
+    throw new ApiError(404, "Tweet not found");
+  }
+
+  const existingLike = await Like.findOne({
+    tweet: tweetId,
+    likedBy: req.user?._id,
+  });
+
+  if(existingLike) {
+    await Like.findOneAndDelete({tweet: tweetId});
+    return res.status(200).json(new ApiResponse(200, "Tweet Unliked"));
+  }
+  else{
+    const like = await Like.create({
+        tweet: tweetId,
+        likedBy: req.user?._id
+    });
+    return res.status(200).json(new ApiResponse(200, like, "Tweet Liked"));
+  }
+});
 
 const getLikedVideos = asyncHandler(async (req, res) => {
-    //TODO: get all liked videos
-})
+    const likedVideos = await Like.aggregate([
+        {
+            $match: {
+                likedBy: new mongoose.Types.ObjectId(req.user?._id),
+                video: {$ne: null}
+            }
+        },
+        {
+            $lookup: {
+                from: "videos",
+                localField: "video",
+                foreignField: "_id",
+                as: "video",
+            },
+        },
+        {
+            $project: {
+                video: 1,
+                likedBy: 1,
+            }
+        }
+    ])
 
-export {
-    toggleCommentLike,
-    toggleTweetLike,
-    toggleVideoLike,
-    getLikedVideos
-}
+    if(!likedVideos){
+        throw new ApiError(500, "Something went wrong, Unable to get liked videos");
+    }
+
+    return res.status(200).json(
+        new ApiResponse(200, likedVideos, "Liked Videos Fetched Successfully")
+    )
+});
+
+export { toggleCommentLike, toggleTweetLike, toggleVideoLike, getLikedVideos };
